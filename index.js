8c2e599fd7d82f7fd65f0478333d6d092e2a576c
Abdulaziz4010 <abdulazizs707ja@gamil.com>
2024-09-05 17:46:42 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+// 1-masala
+
+// let L = prompt("Uzunlikni santimetrda kiriting:");
+
+// let metr = L / 100;
+
+// console.log(alert(metr));
+// 2-masala
+// let m = prompt("To'liq kilogramda kiriting:");
+
+// let tonna = m / 100;
+
+// console.log(alert(tonna));
+
+// let bayt = prompt("Son kiriting");
+
+// let kilobayt = (bayt / 100) * 100;
+
+// console.log(alert(kilobayt));
+
+// 6 - masala
+
+// let o = 73;
+
+// let d = o / 10;
+
+// let c = o % 10;
+
+// console.log(parseInt(d));
+
+// console.log(parseInt(c));
+
+// 7-masala;
+// let a = 22;
+
+// let b = a / 10 + (a % 10);
+
+// console.log(parseInt(b));
+
+// 8 - masala
+
+// let a = 51;
+
+// let c = a / 10;
+
+// let d = a % 10;
+
+// console.log(parseInt(d * 10 + c));
+
+// 9 - masala
+
+// let a = 523;
+
+// let b = a / 100;
+
+// console.log(parseInt(b));
+
+// 10 - masala
+
+// let a = 123;
+
+// let b = a % 10;
+
+// let c = a / 10;
+
+// console.log(parseInt(b));
+
+// console.log(parseInt(c));
+
+// 11 - masala
+
+let a = 123;
+
+let b = a / 100;
+
+let c = a % 100;
+
+console.log(c);
